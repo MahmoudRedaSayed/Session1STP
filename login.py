12219a41b0d22ba6d268cbc88bf474299901b6bf
MahmoudRedaSayed <76118788+MahmoudRedaSayed@users.noreply.github.com>
2022-02-10 10:34:40 +0200

    The login system file

--- login.py
+++ login.py
@@ -1,23 +1,27 @@
-##the data 
-usernames = ["Ahmed", "Sara", "Mahmoud"]
-passwords = ["1234", "5432", "1412"]
-secret = ["Dont like tea", "Can sing", "Speak Chinese"]
-################################################
-Name=input("Enter the name :: ")
-Password=input("Enter the password : ")
-index=0
-for i in usernames:# Ahmed Sara Mahmoud
-    if i == Name:
-        break
-    else:
-        index=index+1
-if index == len(usernames):
-    print("your name is not correct")
-else:
-    if passwords[index] == Password:
-        print(secret[index])
-    else:
-        print("the password is not correct")
-
-
-
+from data import usernames,passwords,secret
+# #remon
+# for i in usernames:#Ahmed Sara Mahmoud
+#     if Name == i:
+#         if passwords[counter]==password:
+#             print("your data is correct")
+#             break
+#     else:
+#         counter+=1
+# #Reem
+# for i in usernames:
+#     if Name == i:
+#         for j in passwords:
+#             if  password ==j:
+#                 print("your data is correct")
+# if Name in usernames :
+#     if password in passwords:
+#         print("your data is correct")
+#anothor sol
+Name=input("Enter your name: ")
+password=input ("Enter your pass : ")
+for i in range(len(usernames)):
+    if Name==usernames[i]:
+        if password==passwords[i]:
+            print("your data is correct")
+        else:
+            print('your password is not')
